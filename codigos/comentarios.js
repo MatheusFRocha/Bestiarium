60746de9ffd944c206fc1dcc2c5527f1905f2da0
pedrocorrea2002 <pedro.rocha@aedb.br>
2021-11-18 23:38:53 -0300

    Responsividade na página monstro

---
+++ codigos/comentarios.js
@@ -0,0 +1,14 @@
+const comentarios = [
+    {
+        "nome": "Zé",
+        "comentario": "Legal"
+    },
+    {
+        "nome": "Luiz",
+        "comentario": "Que bicho feio"
+    },
+    {
+        "nome": "Gustavo",
+        "comentario": "Credo, Deus me livre de achar um desse"
+    }
+]

6ed5a1dc2acfbf8e58e31d826bd4ff84effe8cf4
Pedro Henrique <63562843+pedrocorrea2002@users.noreply.github.com>
2022-11-25 11:56:21 -0300

    Update conexao.php

--- codigos/conexao.php
+++ codigos/conexao.php
@@ -5,12 +5,6 @@ define('SENHA', '');
 define('DB', 'db_bestiarium');
 define('PORT','3307');
 
-// define('HOST', 'instancia-db.cpe2mainejcm.us-east-1.rds.amazonaws.com');
-// define('USUARIO', 'admin');
-// define('SENHA', 'Facul_aw5');
-// define('DB', 'bestiariumdb');
-// define('PORT','3306');
-
 // $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB, PORT) or die ('Não foi possível conectar');
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB, PORT) or die (mysqli_connect_error());
 
